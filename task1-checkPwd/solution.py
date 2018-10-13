ec395fdb0bb606c7ab52940368925f1b6c3fbf1d
PavlikEXD <43291317+PavlikEXD@users.noreply.github.com>
2018-10-13 18:49:50 +0300

    Update solution.py
    Первое ready

--- task1-checkPwd/solution.py
+++ task1-checkPwd/solution.py
@@ -1,3 +1,22 @@
+def check(pwd,word):
+    i=0
+    while i<len(pwd):
+        j=0
+        while j<len(word):
+            if pwd[i]==word[j]:
+                return True
+            j=j+1
+        i=i+1
+    return False
 def solution(pwd):
     # your solution here
+    arr=['abcdefghijklmnopqrstuvwxyz','ABCDEFGHIJKLMNOPQRSTUVWXYZ','0123456789']
+
+    if len(pwd)<10:
+        return False 
+    t=0
+    while t<3:
+        if check(pwd,arr[t])==False:
+            return False
+        t=t+1
     return True
